8727b5c2de0d9c5c277248c41cf5110d71f45f20
Kor de Jong <k.dejong1@uu.nl>
2016-12-07 16:16:08 +0100

    Wip

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# emis_query_executor
+# query_executor
 
 - [Docker Hub repository](https://hub.docker.com/r/pcraster/emis_query_executor/)
--- source/start_production_server.sh
+++ source/start_production_server.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -e
+
+
 docker build -t test/emis_query_executor .
-docker run -p9090:9090 test/emis_query_executor
+docker run -p3031:3031 test/emis_query_executor
