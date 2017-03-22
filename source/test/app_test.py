f30bf6db893eccf039288639ce8709441787032e
Kor de Jong <k.dejong1@uu.nl>
2017-03-22 11:38:43 +0100

    Update test code

--- source/test/app_test.py
+++ source/test/app_test.py
@@ -5,7 +5,8 @@ from emis_query_executor import create_app
 class AppTest(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app("testing")
+        self.app = create_app("test")
+        self.app.config["TESTING"] = True
 
 
     def tearDown(self):
